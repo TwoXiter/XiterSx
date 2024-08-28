73f8199e4e0de6ba53ae951bba2f357bd9252c3c
TwoXiter <xiterdev7@gmail.com>
2024-08-27 23:41:37 -0300

    Update XToshy.lua

--- EZ-KeySystem/XToshy.lua
+++ EZ-KeySystem/XToshy.lua
@@ -41,7 +41,7 @@ end
 
 -- End Configurations
 -- Optionals
-local Service = tabela.Service,
+local Service = tabela.Service
 local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
 local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
 local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
